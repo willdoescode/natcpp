4208c0a45012b45949ab2207cbbe9aa0e7dd3f94
Will Lane <williamlane923@gmail.com>
2021-02-03 12:09:22 -0800

    removed code from main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,6 @@
 #include <string>
 #include "args/cli_args.h"
 #include "utils/types.h"
-#include "utils/string_formatting/string_effects.h"
-#include "utils/string_formatting/string_coloring.h"
 #include "utils/fs/handle_files.h"
 namespace fs = std::filesystem;
 
@@ -19,16 +17,6 @@ int main(int argc, char* argv[]) {
 
   auto cool = new File(f[0], {Directory}, "1", "2", "3", "4", "5", "6");
   std::cout << cool->doit() << std::endl;
-
-  std::cout << bold("hello") << std::endl;
-  std::cout << red(underline("world")) << std::endl;
-  std::cout << yellow("yellow test") << std::endl;
-  std::cout << magenta("magenta test") << std::endl;
-  std::cout << light_blue("light blue test") << std::endl;
-  std::cout << dark_blue("dark blue test") << std::endl;
-  std::cout << grey("grey test") << std::endl;
-  std::cout << green("green test") << std::endl;
-  std::cout << underline("world") << std::endl;
 
   return 0;
 }
